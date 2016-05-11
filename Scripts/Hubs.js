b03570e766547ec8d6b5b5204b0d4973318181cb
cjlovering <cjlovering@130.215.14.76>
2016-05-11 00:00:26 -0400

    Change: updated rate x10, updated number to 30

--- Scripts/Hubs.js
+++ Scripts/Hubs.js
@@ -8,7 +8,7 @@
     
     
     //constants
-    var RATE = 100;//100
+    var RATE = 10;//100
     var BASE_SIZE = 5;
     var LIGHT = ["#ccff66","#FFD700", "#66ccff", "#ff6fcf", "#ff6666", "#72E6DA"];
     var VIBRANT = ["#7FFF00", "#0276FD", "#00FFFF", "#FF1493", "#FF0000"];    

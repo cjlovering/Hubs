9213db6dd013237d18a165fe0ff7944e127b2a78
cjlovering <cjlovering@130.215.14.76>
2016-05-11 08:02:14 -0400

    Testing: n % 2

--- Scripts/Hubs.js
+++ Scripts/Hubs.js
@@ -85,7 +85,7 @@
 	    var vf = (VELOCITY_FACTOR / (n + 2));
 	    
 	    //goal: stop just going in ducking circles
-	    if(true){
+	    if(n%2==0){
 		this.x += Math.round(vf * this.vx * Math.cos(PI180 * this.y));
 		this.y += Math.round(vf * this.vy * Math.cos(PI180 * this.x));
 	    } else {

1d2ecf7ca49eae0a48390950aa7447adfc1878a0
cjlovering <cjlovering@130.215.14.76>
2016-05-11 23:48:37 -0400

    threshold based off both width & h

--- Scripts/Hubs.js
+++ Scripts/Hubs.js
@@ -261,7 +261,7 @@
 	canvas.width = w;
 	canvas.height = h;
 	
-	build_threshold = Math.round((w) * (1/10)); //* (canvas.devicePixelRatio || 1)
+	build_threshold = Math.round(Math.sqrt(util.square(w) + util.square(h))*(1/8)); //* (canvas.devicePixelRatio || 1)
 	break_threshold = 1.3;
     }
 
